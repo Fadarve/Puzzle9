f8df9a4af0b83b4642f22d8a023f0366ff79b6b4
Fadarve <andres.adarve02@gmail.com>
2020-04-10 10:48:28 -0500

    Base del juego, movimiento de las fichas

---
+++ puzzle99/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace puzzle99
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Piece1 = new System.Windows.Forms.Label();
+            this.Piece2 = new System.Windows.Forms.Label();
+            this.Piece3 = new System.Windows.Forms.Label();
+            this.Piece4 = new System.Windows.Forms.Label();
+            this.Piece5 = new System.Windows.Forms.Label();
+            this.Piece6 = new System.Windows.Forms.Label();
+            this.Piece7 = new System.Windows.Forms.Label();
+            this.Piece8 = new System.Windows.Forms.Label();
+            this.PieceVoid = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Piece1
+            // 
+            this.Piece1.AutoSize = true;
+            this.Piece1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Piece1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Piece1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Piece1.Location = new System.Drawing.Point(126, 73);
+            this.Piece1.Name = "Piece1";
+            this.Piece1.Size = new System.Drawing.Size(65, 71);
+            this.Piece1.TabIndex = 0;
+            this.Piece1.Text = "1";
+            this.Piece1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Piece1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
+            this.Piece1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
+            this.Piece1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ObjChangePosition);
+            // 
+            // Piece2
+            // 
+            this.Piece2.AutoSize = true;
+            this.Piece2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Piece2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Piece2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Piece2.Location = new System.Drawing.Point(197, 73);
+            this.Piece2.Name = "Piece2";
+            this.Piece2.Size = new System.Drawing.Size(65, 71);
+            this.Piece2.TabIndex = 0;
+            this.Piece2.Text = "2";
+            this.Piece2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Piece2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
+            this.Piece2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
+            this.Piece2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ObjChangePosition);
+            // 
+            // Piece3
+            // 
+            this.Piece3.AutoSize = true;
+            this.Piece3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Piece3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Piece3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Piece3.Location = new System.Drawing.Point(268, 73);
+            this.Piece3.Name = "Piece3";
+            this.Piece3.Size = new System.Drawing.Size(65, 71);
+            this.Piece3.TabIndex = 2;
+            this.Piece3.Text = "3";
+            this.Piece3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Piece3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
+            this.Piece3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
+            this.Piece3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ObjChangePosition);
+            // 
+            // Piece4
+            // 
+            this.Piece4.AutoSize = true;
+            this.Piece4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Piece4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Piece4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Piece4.Location = new System.Drawing.Point(126, 154);
+            this.Piece4.Name = "Piece4";
+            this.Piece4.Size = new System.Drawing.Size(65, 71);
+            this.Piece4.TabIndex = 3;
+            this.Piece4.Text = "4";
+            this.Piece4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Piece4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
+            this.Piece4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
+            this.Piece4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ObjChangePosition);
+            // 
+            // Piece5
+            // 
+            this.Piece5.AutoSize = true;
+            this.Piece5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Piece5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Piece5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Piece5.Location = new System.Drawing.Point(197, 154);
+            this.Piece5.Name = "Piece5";
+            this.Piece5.Size = new System.Drawing.Size(65, 71);
+            this.Piece5.TabIndex = 4;
+            this.Piece5.Text = "5";
+            this.Piece5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Piece5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
+            this.Piece5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
+            this.Piece5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ObjChangePosition);
+            // 
+            // Piece6
+            // 
+            this.Piece6.AutoSize = true;
+            this.Piece6.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Piece6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Piece6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Piece6.Location = new System.Drawing.Point(268, 154);
+            this.Piece6.Name = "Piece6";
+            this.Piece6.Size = new System.Drawing.Size(65, 71);
+            this.Piece6.TabIndex = 5;
+            this.Piece6.Text = "6";
+            this.Piece6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Piece6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
+            this.Piece6.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
+            this.Piece6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ObjChangePosition);
+            // 
+            // Piece7
+            // 
+            this.Piece7.AutoSize = true;
+            this.Piece7.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Piece7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Piece7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Piece7.Location = new System.Drawing.Point(126, 235);
+            this.Piece7.Name = "Piece7";
+            this.Piece7.Size = new System.Drawing.Size(65, 71);
+            this.Piece7.TabIndex = 6;
+            this.Piece7.Text = "7";
+            this.Piece7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Piece7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
+            this.Piece7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
+            this.Piece7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ObjChangePosition);
+            // 
+            // Piece8
+            // 
+            this.Piece8.AutoSize = true;
+            this.Piece8.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Piece8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Piece8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Piece8.Location = new System.Drawing.Point(197, 235);
+            this.Piece8.Name = "Piece8";
+            this.Piece8.Size = new System.Drawing.Size(65, 71);
+            this.Piece8.TabIndex = 7;
+            this.Piece8.Text = "8";
+            this.Piece8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Piece8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
+            this.Piece8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
+            this.Piece8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ObjChangePosition);
+            // 
+            // PieceVoid
+            // 
+            this.PieceVoid.AutoSize = true;
+            this.PieceVoid.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.PieceVoid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PieceVoid.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PieceVoid.Location = new System.Drawing.Point(268, 235);
+            this.PieceVoid.Name = "PieceVoid";
+            this.PieceVoid.Size = new System.Drawing.Size(64, 71);
+            this.PieceVoid.TabIndex = 8;
+            this.PieceVoid.Text = "  ";
+            this.PieceVoid.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.PieceVoid.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
+            this.PieceVoid.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
+            this.PieceVoid.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ObjChangePosition);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PieceVoid);
+            this.Controls.Add(this.Piece8);
+            this.Controls.Add(this.Piece7);
+            this.Controls.Add(this.Piece6);
+            this.Controls.Add(this.Piece5);
+            this.Controls.Add(this.Piece4);
+            this.Controls.Add(this.Piece3);
+            this.Controls.Add(this.Piece2);
+            this.Controls.Add(this.Piece1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Piece1;
+        private System.Windows.Forms.Label Piece2;
+        private System.Windows.Forms.Label Piece3;
+        private System.Windows.Forms.Label Piece4;
+        private System.Windows.Forms.Label Piece5;
+        private System.Windows.Forms.Label Piece6;
+        private System.Windows.Forms.Label Piece7;
+        private System.Windows.Forms.Label Piece8;
+        private System.Windows.Forms.Label PieceVoid;
+    }
+}
+
---
+++ puzzle99/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzle99
+{
+    public partial class Form1 : Form
+    {
+        private Label[] numbers = new Label[9];
+        private static Point[] locations = new Point[9];
+        public Form1()
+        {
+            InitializeComponent();
+            initialize();
+
+
+        }
+
+        public void initialize()
+        {
+            numbers[0] = Piece1;
+            numbers[1] = Piece2;
+            numbers[2] = Piece3;
+            numbers[3] = Piece4;
+            numbers[4] = Piece5;
+            numbers[5] = Piece6;
+            numbers[6] = Piece7;
+            numbers[7] = Piece8;
+            numbers[8] = PieceVoid;
+
+            for(int i = 0; i < 9; i++)
+            {
+                locations[i] = numbers[i].Location;
+            }
+        }
+
+        private Point MouseDownLocation;
+        private Label selected=null;
+        private Point aux1, aux2;
+        private Label auxLabel;
+        private bool made;
+        private int position;
+        
+        private void ObjClicked(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                MouseDownLocation = e.Location;
+                selected = sender as Label;
+                aux1 = selected.Location;
+                selected.BringToFront();
+                for(int i = 0; i < 9; i++)
+                {
+                    if (aux1 == numbers[i].Location)
+                    {
+                        position = i;
+                    }
+                }
+               
+            }
+        }
+
+        private void ObjMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                selected.Left = e.X + selected.Left - MouseDownLocation.X;
+                selected.Top = e.Y + selected.Top - MouseDownLocation.Y;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ObjChangePosition(object sender, MouseEventArgs e)
+        {
+            made=false;
+            if (selected != null)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if ((selected.Left + e.X) > numbers[i].Left && (selected.Left + e.X) < (numbers[i].Left + numbers[i].Width) && (selected.Top + e.Y) > numbers[i].Top && (selected.Top + e.Y) < (numbers[i].Top + numbers[i].Height) && made==false && i!=position)
+                    {
+                        aux2 = locations[i];
+                        selected.Location = aux2;
+                        numbers[i].Location = aux1;
+                        auxLabel = numbers[i];
+                        numbers[i] = numbers[position];
+                        numbers[position] = auxLabel;
+                        made = true;
+                    }
+                }
+                if (made==false)
+                {
+                    selected.Location = aux1;
+                    made = true;
+                }
+            }
+            selected = null;
+        }
+
+        
+
+      
+
+       
+
+      
+    }
+}
