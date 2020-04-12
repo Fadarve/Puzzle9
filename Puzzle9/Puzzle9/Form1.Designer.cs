97421b1e30b61d9c203088debdb9a7318e6bfa65
Fadarve <andres.adarve02@gmail.com>
2020-04-11 23:31:53 -0500

    Añadida funcion para confirmar la paridad del puzzle

--- Puzzle9/Puzzle9/Form1.Designer.cs
+++ Puzzle9/Puzzle9/Form1.Designer.cs
@@ -28,27 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            
+            this.components = new System.ComponentModel.Container();
             this.CmbTamanyos = new System.Windows.Forms.ComboBox();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.labelInstrucciones = new System.Windows.Forms.Label();
+            this.botonResolver = new System.Windows.Forms.Button();
+            this.labelMovimientos = new System.Windows.Forms.Label();
+            this.labelTiempo = new System.Windows.Forms.Label();
+            this.textoMovimientos = new System.Windows.Forms.TextBox();
+            this.textoTiempo = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
             // 
             // CmbTamanyos
             // 
             this.CmbTamanyos.FormattingEnabled = true;
-            this.CmbTamanyos.Location = new System.Drawing.Point(866, 41);
+            this.CmbTamanyos.Location = new System.Drawing.Point(843, 15);
+            this.CmbTamanyos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.CmbTamanyos.Name = "CmbTamanyos";
-            this.CmbTamanyos.Size = new System.Drawing.Size(121, 24);
+            this.CmbTamanyos.Size = new System.Drawing.Size(145, 24);
             this.CmbTamanyos.TabIndex = 10;
             this.CmbTamanyos.SelectedIndexChanged += new System.EventHandler(this.CmbTamanyos_SelectedIndexChanged);
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.labelTitulo.Location = new System.Drawing.Point(16, -4);
+            this.labelTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(308, 47);
+            this.labelTitulo.TabIndex = 11;
+            this.labelTitulo.Text = "Rompecabezas";
+            this.labelTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelInstrucciones
+            // 
+            this.labelInstrucciones.AutoSize = true;
+            this.labelInstrucciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.labelInstrucciones.Location = new System.Drawing.Point(404, 18);
+            this.labelInstrucciones.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelInstrucciones.Name = "labelInstrucciones";
+            this.labelInstrucciones.Size = new System.Drawing.Size(408, 17);
+            this.labelInstrucciones.TabIndex = 12;
+            this.labelInstrucciones.Text = "Antes de comenzar seleccione el número de piezas que desee:";
+            // 
+            // botonResolver
+            // 
+            this.botonResolver.Location = new System.Drawing.Point(784, 63);
+            this.botonResolver.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.botonResolver.Name = "botonResolver";
+            this.botonResolver.Size = new System.Drawing.Size(199, 28);
+            this.botonResolver.TabIndex = 13;
+            this.botonResolver.Text = "Resolver";
+            this.botonResolver.UseVisualStyleBackColor = true;
+            this.botonResolver.Visible = false;
+            this.botonResolver.Click += new System.EventHandler(this.botonResolver_Click);
+            // 
+            // labelMovimientos
+            // 
+            this.labelMovimientos.AutoSize = true;
+            this.labelMovimientos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.labelMovimientos.Location = new System.Drawing.Point(780, 133);
+            this.labelMovimientos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelMovimientos.Name = "labelMovimientos";
+            this.labelMovimientos.Size = new System.Drawing.Size(121, 24);
+            this.labelMovimientos.TabIndex = 14;
+            this.labelMovimientos.Text = "Movimientos:";
+            this.labelMovimientos.Visible = false;
+            this.labelMovimientos.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // labelTiempo
+            // 
+            this.labelTiempo.AutoSize = true;
+            this.labelTiempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.labelTiempo.Location = new System.Drawing.Point(780, 167);
+            this.labelTiempo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTiempo.Name = "labelTiempo";
+            this.labelTiempo.Size = new System.Drawing.Size(80, 24);
+            this.labelTiempo.TabIndex = 15;
+            this.labelTiempo.Text = "Tiempo:";
+            this.labelTiempo.Visible = false;
+            // 
+            // textoMovimientos
+            // 
+            this.textoMovimientos.BackColor = System.Drawing.SystemColors.Window;
+            this.textoMovimientos.Location = new System.Drawing.Point(917, 130);
+            this.textoMovimientos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textoMovimientos.Name = "textoMovimientos";
+            this.textoMovimientos.ReadOnly = true;
+            this.textoMovimientos.Size = new System.Drawing.Size(71, 22);
+            this.textoMovimientos.TabIndex = 16;
+            this.textoMovimientos.Visible = false;
+            // 
+            // textoTiempo
+            // 
+            this.textoTiempo.BackColor = System.Drawing.SystemColors.Window;
+            this.textoTiempo.Location = new System.Drawing.Point(871, 169);
+            this.textoTiempo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textoTiempo.Name = "textoTiempo";
+            this.textoTiempo.ReadOnly = true;
+            this.textoTiempo.Size = new System.Drawing.Size(117, 22);
+            this.textoTiempo.TabIndex = 17;
+            this.textoTiempo.Visible = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(999, 750);
+            this.Controls.Add(this.textoTiempo);
+            this.Controls.Add(this.textoMovimientos);
+            this.Controls.Add(this.labelTiempo);
+            this.Controls.Add(this.labelMovimientos);
+            this.Controls.Add(this.botonResolver);
+            this.Controls.Add(this.labelInstrucciones);
+            this.Controls.Add(this.labelTitulo);
             this.Controls.Add(this.CmbTamanyos);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Rompecabezas (Grupo 4)";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -57,6 +162,14 @@
 
         #endregion
         private System.Windows.Forms.ComboBox CmbTamanyos;
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.Label labelInstrucciones;
+        private System.Windows.Forms.Button botonResolver;
+        private System.Windows.Forms.Label labelMovimientos;
+        private System.Windows.Forms.Label labelTiempo;
+        private System.Windows.Forms.TextBox textoMovimientos;
+        private System.Windows.Forms.TextBox textoTiempo;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
--- Puzzle9/Puzzle9/Form1.cs
+++ Puzzle9/Puzzle9/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace puzzle99
 {
-    
+
     public partial class Form1 : Form
     {
         private Label[] numbers1 = new Label[81];  //arreglo que almacena todos los labels en orden
@@ -24,19 +24,16 @@ namespace puzzle99
         public void initialize()
         {
             //arreglo que almaccecna los label de cada ficha
-
-
-            CmbTamanyos.Items.Add("3x3");
-            CmbTamanyos.Items.Add("4x4");
-            CmbTamanyos.Items.Add("5x5");
-            CmbTamanyos.Items.Add("6x6");
-            CmbTamanyos.Items.Add("7x7");
-            CmbTamanyos.Items.Add("8x8");
-            CmbTamanyos.Items.Add("9x9");
+            CmbTamanyos.Items.Add("3x3     (9 piezas)");
+            CmbTamanyos.Items.Add("4x4     (16 piezas)");
+            CmbTamanyos.Items.Add("5x5     (25 piezas)");
+            CmbTamanyos.Items.Add("6x6     (36 piezas)");
+            CmbTamanyos.Items.Add("7x7     (49 piezas)");
+            CmbTamanyos.Items.Add("8x8     (64 piezas)");
+            CmbTamanyos.Items.Add("9x9     (81 piezas)");
             n = 3;  //se fija inicialmente el tamano de n en 3
             CreateLabels();
             CmbTamanyos.SelectedIndex = 0;
-           
         }
 
         private Point MouseDownLocation; //posicion en la que se hace click al seleccionar una ficha
@@ -46,14 +43,16 @@ namespace puzzle99
         private bool made;
         private int position;  //auxiliar para indicar la posicion en el arreglo del numero seleccionado
 
+        private bool verificarParidad; //Para verificar la paridad del algoritmo
 
-        //funcion que crea autmaticamente todos los 81 labels necesarios para el tamano maxmo del puzzle
-        public void CreateLabels() 
+
+        //Función que crea automáticamente todos los 81 labels necesarios para el tamaño maxmo del puzzle
+        public void CreateLabels()
         {
-            for(int i=0; i < 81; i++)
+            for (int i = 0; i < 81; i++)
             {
                 Label label = new Label();
-                label.Name = "Piece" + (i+1).ToString();
+                label.Name = "Piece" + (i + 1).ToString();
                 label.AutoSize = false;
                 label.BackColor = System.Drawing.SystemColors.AppWorkspace;
                 label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -61,7 +60,7 @@ namespace puzzle99
                 label.Location = new System.Drawing.Point(240, 40);
                 label.Size = new System.Drawing.Size(50, 50);
                 label.TabIndex = 0;
-                label.Text = (i+1).ToString();
+                label.Text = (i + 1).ToString();
                 label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
                 label.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ObjClicked);
                 label.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ObjMove);
@@ -75,30 +74,31 @@ namespace puzzle99
             numbers1[80].BackColor = System.Drawing.SystemColors.ControlDarkDark;
         }
 
-        //funcion que reinicia la lista, le asigna los numeros dependiendo de n y los dibuja en pantalla
+        //Función que reinicia la lista, le asigna los numeros dependiendo de n y los dibuja en pantalla
         public void DrawPieces()
         {
-            for (int i=0;i<SelectNumbers.Count;i++)
+            for (int i = 0; i < SelectNumbers.Count; i++)
             {
                 SelectNumbers[i].Visible = false;
             }
 
             SelectNumbers.Clear();
 
-            for(int i = 0;i<n*n;i++)
+            for (int i = 0; i < n * n; i++)
             {
-                if (i!=(n*n)-1)
+                if (i != (n * n) - 1)
                 {
                     SelectNumbers.Add(numbers1[i]);
                 }
-                else {
+                else
+                {
                     SelectNumbers.Add(numbers1[80]);
                 }
             }
 
-            int county = 0, posX=40,posY=40;
+            int county = 0, posX = 40, posY = 40;
 
-            for(int i = 0; i < n*n; i++)
+            for (int i = 0; i < n * n; i++)
             {
                 SelectNumbers[i].Location = new System.Drawing.Point(posX, posY);
                 SelectNumbers[i].Visible = true;
@@ -110,31 +110,35 @@ namespace puzzle99
                     posY += 60;
                     county = 0;
                 }
-
             }
         }
-        
-        private void ObjClicked(object sender, MouseEventArgs e)  
+
+        // Dibuja loc controles adicionales del tablero
+        public void drawControlls()
+        {
+            this.botonResolver.Visible = true;
+        }
+
+        private void ObjClicked(object sender, MouseEventArgs e)
         {
             if (e.Button == System.Windows.Forms.MouseButtons.Left)
             {
                 MouseDownLocation = e.Location;
                 selected = sender as Label;
-                aux1 = selected.Location;  //guarda la posicion de la fucha antes de ser movida
+                aux1 = selected.Location;  //guarda la posicion de la ficha antes de ser movida
                 selected.BringToFront();
-                for(int i = 0; i < n*n; i++) //encuentra la posicion en el arreglo de la ficha
+                for (int i = 0; i < n * n; i++) //encuentra la posicion en el arreglo de la ficha
                 {
                     if (aux1 == SelectNumbers[i].Location)
                     {
                         position = i;
                     }
                 }
-               
             }
         }
 
         //funcion que actualiza la posicion de la ficha mientras el mouse se mueve
-        private void ObjMove(object sender, MouseEventArgs e)  
+        private void ObjMove(object sender, MouseEventArgs e)
         {
             if (e.Button == System.Windows.Forms.MouseButtons.Left)
             {
@@ -151,7 +155,7 @@ namespace puzzle99
         //actualiza el valor de n y vuelve a dibujar
         private void CmbTamanyos_SelectedIndexChanged(object sender, EventArgs e)
         {
-            switch(CmbTamanyos.SelectedIndex)
+            switch (CmbTamanyos.SelectedIndex)
             {
                 case 0:
                     n = 3;
@@ -177,18 +181,118 @@ namespace puzzle99
             }
 
             DrawPieces();
+            drawControlls();                    //Dibujar los controles adicionales para el rompecabezas
+        }
+
+
+        //Función para verificar la pariedad de la solución
+        public bool paridadSolucion()
+        {
+            int invert = 0, rowNum = 1, columnCount = 0;
+            int auxPrevio, auxSiguiente;
+            // Conteno de inversiones en las fichas
+            for (int i = 0; i < n * n; i++)
+            {
+                if (SelectNumbers[i].Text != " ")
+                {
+                    for (int j = i + 1; j < n * n; j++)
+                    {
+                        auxPrevio = int.Parse(SelectNumbers[i].Text);           // Auxiliar de la etiqueta de la posición previa convertido de Label a Int
+                        if (SelectNumbers[j].Text != " ")
+                        {
+                            auxSiguiente = int.Parse(SelectNumbers[j].Text);    // Auxiliar de la etiqueta de la posición siguiente convertido de Label a Int
+                            if (auxPrevio > auxSiguiente)
+                            {
+                                invert++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                columnCount++;
+                if (columnCount == n + 1)
+                {
+                    columnCount = 0;
+                    rowNum++;
+                }
+                if (SelectNumbers[i].Text == " ")
+                {
+                    break;
+                }
+            }
+
+
+            // CONDICIONES
+            // Si N es impar y el número de inversiones es par en el estado de entrada.
+            if (n % 2 == 1 && invert % 2 == 0)
+            {
+                verificarParidad = true;                    // SOLUCIONABLE
+            }
+            else
+
+            // Si N es par, el blanco está en una fila par y el numero de inversiones es impar
+            if (n % 2 == 0 && rowNum % 2 == 0 && invert % 2 == 1)
+            {
+                verificarParidad = true;                    // SOLUCIONABLE
+            }
+            else
+
+            // Si N es par, el blanco está en una fila impar y el número de inversiones es par. 
+            if (n % 2 == 0 && rowNum % 2 == 1 && invert % 2 == 0)
+            {
+                verificarParidad = true;                    // SOLUCIONABLE
+            }
+
+            // Cuando no se cumple ninguna de las condiciones anteriores 
+            else
+            {
+                verificarParidad = false;                   // NO SOLUCIONABLE
+            }
+
+            return verificarParidad;
+        }
+
+        //Para que sea visible el mensaje de verificación de paridado
+        private void botonResolver_Click(object sender, EventArgs e)
+        {
+            paridadSolucion();
+
+            if (verificarParidad == false)
+            {
+                MessageBox.Show("No es posible resolver este rompecabeza. Intente acomodar las fichas nuevamente");
+            }
+            else
+            {
+                this.labelMovimientos.Visible = true;
+                this.textoMovimientos.Visible = true;
+                this.labelTiempo.Visible = true;
+                this.textoTiempo.Visible = true;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
         }
 
         private void ObjChangePosition(object sender, MouseEventArgs e)
         {
-            made=false;
+            made = false;
             if (selected != null)
             {
-                for (int i = 0; i < n*n; i++)
+                for (int i = 0; i < n * n; i++)
                 {
                     /* condicional que verifica que cuando el mouse sea levantado, 
                      * sea dentro de los limites de alguna de las fichas diferentes a la seleccionada*/
-                    if ((selected.Left + e.X) > SelectNumbers[i].Left && (selected.Left + e.X) < (SelectNumbers[i].Left + SelectNumbers[i].Width) && (selected.Top + e.Y) > SelectNumbers[i].Top && (selected.Top + e.Y) < (SelectNumbers[i].Top + SelectNumbers[i].Height) && made==false && i!=position)
+                    if ((selected.Left + e.X) > SelectNumbers[i].Left && (selected.Left + e.X) < (SelectNumbers[i].Left + SelectNumbers[i].Width) && (selected.Top + e.Y) > SelectNumbers[i].Top && (selected.Top + e.Y) < (SelectNumbers[i].Top + SelectNumbers[i].Height) && made == false && i != position)
                     {
                         //se actualizan las posiciones de los labels visualmente y en el arreglo
 
@@ -204,13 +308,13 @@ namespace puzzle99
                 }
                 /* si no se encontró concordancia con ninguna de las otras fichas, 
                    devuelve la seleccionada a su posicion inicial*/
-                if (made==false)
+                if (made == false)
                 {
                     selected.Location = aux1;
                     made = true;
                 }
             }
             selected = null;
-        } 
+        }
     }
 }
